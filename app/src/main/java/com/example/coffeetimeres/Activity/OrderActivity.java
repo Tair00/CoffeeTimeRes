b8ef46d8f94b012f4e7b70ec926fe10c5e83c622
Tair00 <suleimanov.t.i.20@gmail.com>
2024-04-23 20:07:38 +0300

    FireBase notification

--- app/src/main/java/com/example/coffeetimeres/Activity/OrderActivity.java
+++ app/src/main/java/com/example/coffeetimeres/Activity/OrderActivity.java
@@ -1,10 +1,15 @@
 package com.example.coffeetimeres.Activity;
 
 import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -20,6 +25,12 @@ import com.example.coffeetimeres.Adapter.ApprovedBookingListAdapter;
 import com.example.coffeetimeres.Adapter.BookingListAdapter;
 import com.example.coffeetimeres.Domain.BookingItem;
 import com.example.coffeetimeres.R;
+import com.example.coffeetimeres.notificasion.PushService;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.messaging.FirebaseMessaging;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -35,16 +46,18 @@ public class OrderActivity extends Activity {
     private BookingListAdapter adapter;
 
     private List<BookingItem> bookingList;
-
+    FirebaseAnalytics mFirebaseAnalytics;
     private List<BookingItem> approvedBookingList;
     private ApprovedBookingListAdapter secondAdapter;
     private RecyclerView secondRecyclerView;
+    private BroadcastReceiver pushBroadcastReceiver;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
         String token = getIntent().getStringExtra("access_token");
-
+        FirebaseApp.initializeApp(this);
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
         recyclerView = findViewById(R.id.view);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         bookingList = new ArrayList<>();
@@ -56,10 +69,54 @@ public class OrderActivity extends Activity {
         approvedBookingList = new ArrayList<>();
         secondAdapter = new ApprovedBookingListAdapter(OrderActivity.this, approvedBookingList);
         secondRecyclerView.setAdapter(secondAdapter);
+        FirebaseMessaging.getInstance().getToken()
+                .addOnCompleteListener(new OnCompleteListener<String>() {
+                    @Override
+                    public void onComplete(@NonNull Task<String> task) {
+                        if (!task.isSuccessful()) {
+                            return;
+                        }
 
+                        String token = task.getResult();
+                        Log.e("Token", "token -> " + token);
+                    }
+                });
+
+        pushBroadcastReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                Bundle extras = intent.getExtras();
+                Log.e("TAG", "MESSAGE_RECEIVED ");
+                if (extras != null) {
+                    String key = extras.keySet().iterator().next();
+                    if (key.equals(PushService.KEY_ACTION)) {
+                        Log.e("TAG", "KEY  -> " + key);
+                        String action = extras.getString(key);
+                        if (action != null) {
+                            if (action.equals(PushService.ACTIONS_SHOW_MESSAGE)) {
+                                String message = extras.getString(PushService.KEY_MESSAGE);
+                                if (message != null) {
+                                    Log.e("TAG", "MESSAGE_KEY  -> " + message);
+                                    Toast.makeText(getApplicationContext(),message,Toast.LENGTH_SHORT).show();
+                                }
+                            } else {
+                                Log.e("TAG", "NO_");
+                            }
+                        }
+                    }
+                }
+            }
+        };
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(PushService.INTENT_FILTER);
+        registerReceiver(pushBroadcastReceiver, intentFilter);
         executeGetRequest();
     }
-
+    @Override
+    protected void onDestroy() {
+        unregisterReceiver(pushBroadcastReceiver);
+        super.onDestroy();
+    }
     private void executeGetRequest() {
         String token = getIntent().getStringExtra("access_token"); // Получение значения токена
         RequestQueue queue = Volley.newRequestQueue(this);
